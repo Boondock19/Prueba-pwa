4c40e26b4123315ed3097766bbdd74051ee1a72f
Boondock19 <josematiasgonzalez20@gmail.com>
2022-11-18 17:42:24 -0400

    validaciones del blur y parseAmount

--- prueba-pwa/src/app/components/login/login.component.ts
+++ prueba-pwa/src/app/components/login/login.component.ts
@@ -69,4 +69,88 @@ export class LoginComponent implements OnInit {
     console.log('entra con el evento',e)
     e.target.value = e.target.value.replace(/[^0-9]/ig, '');
     }
+
+     /**
+   * Método utilizado para parsear los montos
+   * @param str monto ingresado en el control amount
+   */
+  parseAmount(e:any) {
+
+
+    e.preventDefault();
+    let str = e.target.value;
+
+    //Quittamos todo los puntos y comas del string para tenrlo limpio
+    str = str.replace(/\,/g, '');
+    str = str.replace(/\./g, '')
+
+
+    if (!isNaN(parseInt(e.key)) && !(str === "000" && e.key === "0") && str.length < 15) {
+      str += e.key
+      if (str[0] === "0") str = str.substring(1);
+    }
+    else if (e.key === "Backspace" && str !== "000") {
+      str = str.slice(0, -1)
+      if (str.length < 3) str = "0" + str
+    }
+    else return;
+
+    //Creamos un array con cada elemento del string
+    let amount = str.split('');
+
+    //Se coloca una punto para marcar los 2 decimales
+    amount.splice(-2, 0, '.');
+
+    //Se convierte el array en string con los decimales ya puestos
+    let numberWD = amount.toString();
+    numberWD = numberWD.replace(/\,/g, '');
+
+    //Separamos parte entera y parte decimal
+    let aux = numberWD.split('.');
+    let intNumber = aux[0].split('').reverse();
+    let dcNumber = aux[1];
+
+    //Iniicalizamos array para guardar el numero con los puntos de separacion
+    let dotNumberResult:any[] = []
+    let cont = 0;
+
+    //Pusheamos los puntos de separacion cada 3 numeros
+    intNumber.forEach((number:any) => {
+
+      if (cont % 3 == 0 && cont != 0) {
+        dotNumberResult.push('.')
+      }
+
+      cont++;
+      dotNumberResult.push(number);
+
+    })
+
+    dotNumberResult.reverse();
+
+    //Convertimos las parte entera con los separadores a string
+    let result = dotNumberResult.toString();
+    result = result.replace(/\,/g, '');
+
+    //Juntamos parte entera y parte decimal
+    result = result + ',' + dcNumber;
+
+    this.loginForm.controls['password'].setValue(result);
+  }
+
+   /**
+   *
+   */
+    validateAmount() {
+
+      let amount: string = this.loginForm.value.amount
+  
+      if (amount == '0,00' || amount.length < 4) {
+  
+        this.loginForm.controls['amount'].setErrors({ 'novalidate': true })
+      }
+  
+    }
+  
+
 }
